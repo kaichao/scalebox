7cc3884b2807237cd3fe1a1730d2e81488e61c81
kaichao <wukaichao@gmail.com>
2024-05-26 20:30:13 +0800

    add GetDB() in golang code.

--- golang/misc/sql.go
+++ golang/misc/sql.go
@@ -1,6 +1,13 @@
 package misc
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/kaichao/scalebox/golang/misc"
+)
 
 // NewSQLNullString ...
 func NewSQLNullString(s string) sql.NullString {
@@ -11,4 +18,33 @@ func NewSQLNullString(s string) sql.NullString {
 		String: s,
 		Valid:  true,
 	}
+}
+
+var (
+	db *sql.DB
+)
+
+// GetDB ...
+func GetDB() *sql.DB {
+	if db == nil {
+		dbHost := os.Getenv("PGHOST")
+		if dbHost == "" {
+			dbHost = misc.GetLocalIP()
+		}
+		dbPort := os.Getenv("PGPORT")
+		if dbPort == "" {
+			dbPort = "5432"
+		}
+		databaseURL := fmt.Sprintf("postgres://scalebox:changeme@%s:%s/scalebox", dbHost, dbPort)
+		// set database connection
+		var err error
+		if db, err = sql.Open("pgx", databaseURL); err != nil {
+			log.Fatal("Unable to connect to database:", err)
+		}
+		db.SetConnMaxLifetime(500)
+		db.SetMaxIdleConns(50)
+		db.SetMaxOpenConns(20)
+		// db.Stats()
+	}
+	return db
 }
