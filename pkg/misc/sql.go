7424a254e38a9f1bb7453d1eb469ac057f30173b
kaichao <wukaichao@gmail.com>
2024-05-28 14:36:34 +0800

    update misc.getDB() in golang pkg.

--- pkg/misc/sql.go
+++ pkg/misc/sql.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -25,37 +26,63 @@ var (
 
 // GetDB ...
 func GetDB() *sql.DB {
-	if db == nil {
-		var databaseURL string
-		if databaseURL = os.Getenv("DATABASE_URL"); len(databaseURL) == 0 {
-
-			dbHost := os.Getenv("PGHOST")
-			dbPort := os.Getenv("PGPORT")
-			if dbHost == "" {
-				dbHost = os.Getenv("LOCAL_ADDR")
-				if dbHost == "" {
-					dbHost = GetLocalIP()
-				}
-			}
-			ss := strings.Split(dbHost, ":")
-			if len(ss) == 2 {
-				dbHost = ss[0]
-				dbPort = ss[1]
-			}
-			if dbPort == "" {
-				dbPort = "5432"
-			}
-			databaseURL = fmt.Sprintf("postgres://scalebox:changeme@%s:%s/scalebox", dbHost, dbPort)
-		}
-		// set database connection
-		var err error
-		if db, err = sql.Open("pgx", databaseURL); err != nil {
-			log.Fatal("Unable to connect to database:", err)
-		}
-		db.SetConnMaxLifetime(500)
-		db.SetMaxIdleConns(50)
-		db.SetMaxOpenConns(20)
-		// db.Stats()
+	if db != nil {
+		return db
 	}
+
+	databaseURL := os.Getenv("DATABASE_URL")
+	if len(databaseURL) == 0 {
+		pgHost := os.Getenv("PGHOST")
+		pgPort := os.Getenv("PGPORT")
+		if pgHost == "" {
+			pgHost = os.Getenv("LOCAL_ADDR")
+			if pgHost == "" {
+				pgHost = GetLocalIP()
+			}
+		}
+		// ${PGHOST}:${PGPORT}
+		ss := strings.Split(pgHost, ":")
+		if len(ss) == 2 {
+			pgHost = ss[0]
+			pgPort = ss[1]
+		}
+		if pgPort == "" {
+			pgPort = "5432"
+		}
+		pgUser := os.Getenv("PGUSER")
+		if len(pgUser) == 0 {
+			pgUser = "scalebox"
+		}
+		pgPass := os.Getenv("PGPASS")
+		if len(pgPass) == 0 {
+			pgPass = "changeme"
+		}
+		pgDB := os.Getenv("PGDB")
+		if len(pgDB) == 0 {
+			pgDB = "scalebox"
+		}
+
+		databaseURL = fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+			pgUser, pgPass, pgHost, pgPort, pgDB)
+	}
+	s := os.Getenv("PG_MAX_IDLE_CONNS")
+	maxIdles, _ := strconv.Atoi(s)
+	if maxIdles <= 0 {
+		maxIdles = 50
+	}
+	s = os.Getenv("PG_MAX_OPEN_CONNS")
+	maxOpens, _ := strconv.Atoi(s)
+	if maxOpens <= 0 {
+		maxOpens = 20
+	}
+	// set database connection
+	var err error
+	if db, err = sql.Open("pgx", databaseURL); err != nil {
+		log.Fatal("Unable to connect to database:", err)
+	}
+	db.SetConnMaxLifetime(500)
+	db.SetMaxIdleConns(maxIdles)
+	db.SetMaxOpenConns(maxOpens)
+	// db.Stats()
 	return db
 }
