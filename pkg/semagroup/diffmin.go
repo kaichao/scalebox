f0da8171943d6c4d73183be59bdfb92e240cf35c
kaichao <wukaichao@gmail.com>
2025-09-09 10:45:01 +0800

    Fix bug in pkg/semagroup/DiffMin.

--- pkg/semagroup/diffmin.go
+++ pkg/semagroup/diffmin.go
@@ -7,13 +7,15 @@ import (
 )
 
 // DiffMin 计算当前信号量值与信号量组最小值的差值
-// semaEXpr:正则表达式，其形式为(group-prefix)expr，其中圆括号中group-prefix标识为信号量组的前缀
+// semaExpr:正则表达式，其形式为(group-prefix)expr，其中圆括号中group-prefix标识为信号量组的前缀
 func DiffMin(semaExpr string, appID int) (int, error) {
 	// 解析group表达式
 	groupExpr, expr, err := parseGroupExpr(semaExpr)
 	if err != nil {
 		return 0, err
 	}
+
+	fmt.Printf("group-expr:%s,current-expr:%s,app-id:%d.\n", groupExpr, expr, appID)
 
 	// 在事务中执行查询
 	tx, err := postgres.GetDB().Begin()
@@ -27,7 +29,8 @@ func DiffMin(semaExpr string, appID int) (int, error) {
 	err = tx.QueryRow(`
 		SELECT value 
 		FROM t_semaphore 
-		WHERE name ~ $1 AND app = $2`,
+		WHERE name = $1 AND app = $2
+	`,
 		expr, appID).Scan(&currentValue)
 	if err != nil {
 		return 0, fmt.Errorf("failed to query current semaphore value (expr:%s, appID:%d): %w", expr, appID, err)
@@ -38,7 +41,8 @@ func DiffMin(semaExpr string, appID int) (int, error) {
 	err = tx.QueryRow(`
 		SELECT MIN(value)
 		FROM t_semaphore
-		WHERE name ~ $1 AND app = $2`,
+		WHERE name ~ $1 AND app = $2
+	`,
 		groupExpr, appID).Scan(&minValue)
 	if err != nil {
 		return 0, fmt.Errorf("failed to query min semaphore value: %w", err)
